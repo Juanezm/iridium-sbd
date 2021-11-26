9b7e3cab30eefc25a905dfed515ef1bd606bba4c
Juan Emilio Zurita Macias <juan.macias@sa.catapult.org.uk>
2021-11-26 16:39:40 +0000

    Replace class with adafruit rockblock library

--- src/iridium_sbd/cli.py
+++ src/iridium_sbd/cli.py
@@ -1,7 +1,30 @@
 import click
 import serial
+import time
 
-from iridium_sbd.iridium_9603 import Iridium9603
+from adafruit_rockblock import RockBlock
+
+
+device_info = dict()
+
+
+def update_device_info(rb: RockBlock):
+    device_info['Status'] = rb.status
+    device_info['Model'] = rb.model
+    for item in rb.revision:
+        elements = item.split(':')
+        device_info[elements[0]] = elements[1][1:]
+
+    device_info['Serial Number'] = rb.serial_number
+    device_info['Geolocation'] = f"{rb.geolocation[:-1]} {time.strftime('%Y-%m-%dT%H:%M:%SZ', rb.geolocation[-1])}"
+    device_info['System time'] = time.strftime(
+        '%Y-%m-%dT%H:%M:%SZ', rb.system_time)
+
+
+def print_device_info():
+    for item in device_info.items():
+        k, v = item
+        print("{:<23} | {:<15}".format(k, str(v)))
 
 
 @click.group()
@@ -14,20 +37,63 @@ def cli(ctx, serial_device: str):
                        parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
                        xonxoff=False, timeout=1) as serial_conn:
 
-        iridium = Iridium9603(serial=serial_conn)
-
-        for item in iridium.__dict__.items():
-            k, v = item
-            if '_' not in k:
-                print("{:<12} | {:<15}".format(k, str(v)))
+        rb = RockBlock(serial_conn)
+        update_device_info(rb)
+        print_device_info()
 
 
 @cli.command()
+@click.option('--message', default="Hello world from space!", help='Message to send.')
 @click.pass_context
-def signal_strength(ctx):
+def send(ctx, message):
     with serial.Serial(ctx.parent.serial_device, baudrate=19200, bytesize=serial.EIGHTBITS,
                        parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
                        xonxoff=False, timeout=1) as serial_conn:
 
-        iridium = Iridium9603(serial=serial_conn)
-        print(f"\nSignal Strength: {iridium.signal_strength()}")
+        rb = RockBlock(serial_conn)
+        # set the text
+        rb.text_out = message
+
+        # try a satellite Short Burst Data transfer
+        print("Talking to satellite...")
+        status = rb.satellite_transfer()
+        # loop as needed
+        retry = 0
+        while status[0] > 8:
+            time.sleep(10)
+            status = rb.satellite_transfer()
+            print(retry, status)
+            retry += 1
+
+        print("\nDONE.")
+
+
+@cli.command()
+@click.pass_context
+def signal(ctx):
+    with serial.Serial(ctx.parent.serial_device, baudrate=19200, bytesize=serial.EIGHTBITS,
+                       parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+                       xonxoff=False, timeout=1) as serial_conn:
+
+        rb = RockBlock(serial_conn)
+
+        while True:
+
+            resp = rb._uart_xfer("+CSQ")
+
+            if resp[-1].strip().decode() == "OK":
+                signal_level = int(resp[-3].strip().decode().split(":")[1])
+            else:
+                signal_level = 0
+
+            click.clear()
+            print_device_info()
+            print('\nPress CTR+C to exit\n')
+            with click.progressbar(label='Signal', empty_char=' ', length=5, show_pos=True) as bar:
+                bar.update(signal_level)
+
+            time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    cli()
